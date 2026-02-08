eafa3ab1eee415c9f98b3516733defac684fee51
Andres Saray <andres.saray@dualbootpartners.com>
2026-02-08 15:25:16 -0500

    chore: add api key

--- inject-env.sh
+++ inject-env.sh
@@ -3,5 +3,6 @@
 cat <<EOF > /app/dist/env-config.js
 window.ENV = {
   VITE_API_URL: "${VITE_API_URL}"
+  VITE_API_STORES_KEY: "${VITE_API_STORES_KEY}"
 };
 EOF
--- src/api/index.ts
+++ src/api/index.ts
@@ -5,15 +5,23 @@ const getApiUrl = () => {
     return (window as any).ENV.VITE_API_URL
   }
   return import.meta.env.VITE_API_URL || 'http://localhost:3002/api'
-  // return 'http://localhost:3002/api'
+}
+
+const getApiKey = () => {
+  if (typeof window !== 'undefined' && (window as any).ENV?.VITE_API_STORES_KEY) {
+    return (window as any).ENV.VITE_API_STORES_KEY
+  }
+  return import.meta.env.VITE_API_STORES_KEY || '1234'
 }
 
 const API_URL = getApiUrl()
+const API_KEY = getApiKey()
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
     "x-store-domain": window.location.hostname,
+    "x-api-stores": API_KEY
   },
 })
 
